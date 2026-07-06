8ef76bf407c12dfd68398d93d8b7212bd6bd715b
crowdingcoder <crowdingcoder@192.168.1.24>
2026-07-05 22:18:26 -0400

    Programilogrado

--- functions.py
+++ functions.py
@@ -4,20 +4,16 @@ import time
 
 #Listas
 planes = {"F001":["Plan Básico","mensual",1, False, False, "libre"],
-          "F002":["Plan Full","mensual", 1, False, False, "libre"]}
+          "F002":["Plan Full","mensual", 1, False, False, "libre"],
+          "F003":["Plan Caquita","mensual", 1, False, False, "libre"]}
 inscripciones = {"F001":[14990, 30],
-                 "F002":[22990, 10]}
+                 "F002":[22990, 10],
+                 "F003":[25990, 10]}
 
 def limpiar():
     os.system("clear")
 
 #validaciones
-
-def validar_lista(lista): #como hay 2 listas usare verdadero o falso solo para validar si se puede realizar la operacion y desde main entregar un mensaje ya sea para planes o para inscripciones
-    if len(lista) <= 0:
-        return False
-    else:
-        return True
 
 def validar_string(string):
     if len(string) > 0 and string != " ":
@@ -25,11 +21,6 @@ def validar_string(string):
     else:
         print("El campo no puede ir vacio o solo ser un espacio.")
 
-def validar_entero(entero):
-    if entero > 0:
-        return True
-    else:
-        print("El valor ingresado no puede ser menor o igual a 0.")
 
 #Opcion 1
 def cupos_tipo(tipo):
@@ -39,15 +30,109 @@ def cupos_tipo(tipo):
             acumulador_cupos = acumulador_cupos + inscripciones[x][1]
     print(f"\nEl total de cupos disponibles es: {acumulador_cupos}.")
         
+#Opcion 2
+def busqueda_precio(p_min, p_max):
+    busqueda = []
+    for x in inscripciones:
+        if p_min <= inscripciones[x][0] and p_max >= inscripciones[x][0] and inscripciones[x][1] > 0:
+            busqueda.append(f"{planes[x][0]}--{x}")
+    if len(busqueda) > 0:
+        busqueda.sort()
+        print(f"Los planes encontrados son : {busqueda}")
+    else:
+        print("No se han encontrado planes en ese rango")
 
-
-
+#Opcion 3
+def actualizar_precio(codigo,nuevo_precio):
+    for x in inscripciones:
+        if codigo == x:
+            inscripciones[x][0] = nuevo_precio
+            return True
+        else:
+            return False
+        
 #opcion 4
+def validar_codigo(codigo):
+    
+    if len(codigo) == 0 or codigo == " ":
+        print("El codigo no puede ir vacio")
+        return False
+    for x in planes:
+        if codigo == x:
+            print("El codigo ingresado ya se encuentra registrado.")
+            return False
+    return True
 
-def agregar_plan():
-    while True:
-        codigo = input("Ingrese codigo plan:\n")
-        if validar_string(codigo):
+def validar_nombre(nombre):
+        if len(nombre) == 0 or nombre == " ":
+            print("El nombre no puede ir vacio")
+            return False
+        else:
+            return True
+        
+def validar_tipo(tipo):
+    if tipo == "mensual" or tipo == "trimestral" or tipo == "anual":
+        return True
+    else:
+        print("El tipo de plan solo puede ser: mensual, trimestral o anual.")
+        return False
+    
+def validar_duracion(duracion):
+    if duracion <= 0:
+        print("La duracion no puede ser menor o igual a 0.")
+        return False
+    else:
+        return True
+    
+def validar_acceso_piscina(acceso_piscina):
+    if acceso_piscina == "S":
+        return True
+    elif acceso_piscina == "N":
+        return True
+    else:
+        print("Opcion no valida.")
+        return False
 
-            break
+def validar_incluye_clases(incluye_clases):
+    if incluye_clases == "S":
+        return True
+    elif incluye_clases == "N":
+        return True
+    else:
+        print("Opcion no valida.")
+        return False
+    
+def validar_horario(horario):
+        if len(horario) == 0 or horario == " ":
+            print("El horario no puede ir vacio")
+            return False
+        else:
+            return True
+
+def validar_precio(precio):
+    if precio < 0:
+        print("El precio no puede ser menor que 0.")
+        return False
+    else:
+        return True
+
+def validar_cupos(cupos):
+    if cupos <= 0:
+        print("Los cupos no puede ser menor o igual a 0.")
+        return False
+    else:
+        return True
+
+def agregar_plan(codigo, nombre, tipo, duracion, acceso_piscina, incluye_clases, horario, precio, cupos):
+    planes[codigo] = [nombre, tipo, duracion, acceso_piscina, incluye_clases, horario]
+    inscripciones[codigo] = [precio, cupos]
+
+#opcion 5
+def eliminar_plan(codigo):
+    for x in planes:
+        if codigo == x:
+            planes.pop(x)
+            inscripciones.pop(x)
+            return True
+    return False
 
--- main.py
+++ main.py
@@ -14,16 +14,109 @@ while True:
     menu = int(input("Ingrese opcion:\n"))
     if menu == 1:
         print("Menu 1")
-        tipo = input("Ingrese tipo de plan a consultar:").lower()
+        tipo = input("Ingrese tipo de plan a consultar: ").lower()
         cupos_tipo(tipo)
+
     elif menu == 2:
         print("Menu 2")
+        while True:
+            try:
+                p_min = int(input("Ingrese precio minimo: "))
+                p_max = int(input("Ingrese precio maximo: "))
+                if p_min < 0 or p_max < 0:
+                    print("Los valores ingresados no pueden ser menores a 0.")
+                else:
+                    busqueda_precio(p_min, p_max)
+                    break
+            except:
+                print("Debe ingresar un  valor entero")
+
     elif menu == 3:
         print("menu 3")
+        while True:
+            codigo = input("Ingrese codigo del plan: ").upper()
+            try:
+                nuevo_precio = int(input("Ingrese nuevo precio: "))
+                if nuevo_precio <= 0:
+                    print("El valor ingresado no debe ser menor o igual a 0.")
+                else:
+                    if actualizar_precio(codigo,nuevo_precio):
+                        print("Precio actualizado")
+                    else:
+                        print("El código no existe")
+            except:
+                print("El valor debe ser entero")
+            
+            continuar = input("¿Desea actualizar otro precio (s/n)?").upper()
+            while continuar != "N" and continuar != "S":
+                continuar = input("Opcion no valida.\n¿Desea actualizar otro precio (s/n)? ").upper()
+            if continuar == "N":
+                break
+            elif continuar == "S":
+                continue
+
     elif menu == 4:
         print("Menu 4")
+        while True:
+            codigo = input("Ingrese codigo del plan: ").upper()
+            if validar_codigo(codigo) == False:
+                break
+            nombre = input("Ingrese nombre del plan :")
+            if validar_nombre(nombre) == False:
+                break
+            tipo = input("Ingrese tipo (mensual/trimestral/anual): ").lower()
+            if validar_tipo(tipo) == False:
+                break
+            try:
+                duracion = int(input("Ingrese duración (meses): "))
+                if validar_duracion == False:
+                    break
+            except:
+                print("El valor ingresado solo puede ser entero.")
+                break
+            acceso_piscina = input("¿Incluye acceso a piscina? (s/n): ").upper()
+            if validar_acceso_piscina(acceso_piscina) == False:
+                break
+            if acceso_piscina == "S":
+                acceso_piscina = True
+            elif acceso_piscina == "N":
+                acceso_piscina = False
+            incluye_clases = input("¿Incluye clases grupales? (s/n): ").upper()
+            if validar_incluye_clases == False:
+                break
+            if incluye_clases == "S":
+                incluye_clases = True
+            elif incluye_clases == "N":
+                incluye_clases = False
+            horario = input("Ingrese horario: ").lower()
+            if validar_horario == False:
+                break
+            try:
+                precio = int(input("Ingrese precio: "))
+                if validar_precio(precio) == False:
+                    break
+                cupos= int(input("Ingrese cupos: "))
+                if validar_cupos == False:
+                    break
+            except:
+                print("El valor ingresado debe ser entero.")
+                break
+            agregar_plan(codigo, nombre, tipo, duracion, acceso_piscina, incluye_clases, horario, precio, cupos)
+            print("Plan agregado")
+            print(planes)
+            print(inscripciones)
+            break
+
     elif menu == 5:
         print("Menu 5")
+        codigo = input("Ingrese codigo del plan: ").upper()
+        if eliminar_plan(codigo):
+            print("Plan eliminado")
+            print(planes)
+            print(inscripciones)
+        else:
+            print("El codigo no existe")
+
     elif menu == 6:
         limpiar()
         print("programa finalizado.")
